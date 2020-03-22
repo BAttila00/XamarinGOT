adbee5b5d855fb56d1bd70f1bcfb168b2e4634bd
BAttila <attila.bruder@business-solution.hu>
2020-03-22 22:38:42 +0100

    creating basic view

--- XamarinGOT/XamarinGOT/MainPage.xaml.cs
+++ XamarinGOT/XamarinGOT/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -13,9 +14,25 @@ namespace XamarinGOT
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<string> Categories { get; set; } = new ObservableCollection<string>();
+        public ObservableCollection<string> Characters { get; set; } = new ObservableCollection<string>();
         public MainPage()
         {
             InitializeComponent();
+            Categories.Add("Books");
+            Categories.Add("Characters");
+            Categories.Add("Houses");
+
+            Characters.Add("Jon Snow");
+            Characters.Add("Jaime Lannister");
+
+            CategoryPicker.ItemsSource = Categories;
+            ResultList.ItemsSource = Characters;
+        }
+
+        private void SearchTapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HouseDetails());
         }
     }
 }
