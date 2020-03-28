1e645e6095270e15f76ae9453cd89104fc281a16
BAttila <attila.bruder@business-solution.hu>
2020-03-28 22:13:42 +0100

    Navigation to book details, filling up fields with info using API.

--- XamarinGOT/XamarinGOT/BookDetails.xaml.cs
+++ XamarinGOT/XamarinGOT/BookDetails.xaml.cs
@@ -1,20 +1,55 @@
-﻿using System;
+﻿using GameofThrones.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinGOT.Models;
 
-namespace XamarinGOT
-{
+namespace XamarinGOT {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class BookDetails : ContentPage
-    {
-        public BookDetails()
-        {
+    public partial class BookDetails : ContentPage {
+        private Book _book;
+        public ObservableCollection<Character> CharactersInBook { get; set; } = new ObservableCollection<Character>();
+        public ObservableCollection<string> Authors { get; set; } = new ObservableCollection<string>();
+        public Book Book {
+            get { return _book; }
+            set { _book = value; }
+        }
+        public BookDetails() {
             InitializeComponent();
+        }
+
+        public BookDetails(string url) {
+            InitializeComponent();
+            FillViewParameters(url);
+        }
+
+        public async void FillViewParameters(string url) {
+            var service = new GOTService();
+            Book = await service.GetBookAsync(url);
+
+            BookTitle.Text = Book.name;
+            BookIsbn.Text = Book.isbn;
+            BookPublisher.Text = Book.publisher;
+            BookCountry.Text = Book.country;
+            BookReleased.Text = Book.released.ToString();
+
+            foreach (var author in Book.authors) {
+                Authors.Add(author);
+            }
+            BookAuthors.ItemsSource = Authors;
+
+            var charactersInBook = await service.GetCharactersInBook(Book.characters);
+            foreach (var character in charactersInBook) {
+                CharactersInBook.Add(character);
+            }
+
+            BookCharacters.ItemsSource = CharactersInBook;
         }
     }
 }
--- XamarinGOT/XamarinGOT/MainPage.xaml.cs
+++ XamarinGOT/XamarinGOT/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameofThrones.Services;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -6,36 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using XamarinGOT.Models;
 
-namespace XamarinGOT
-{
+namespace XamarinGOT {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
-    {
+    public partial class MainPage : ContentPage {
         public ObservableCollection<string> Categories { get; set; } = new ObservableCollection<string>();
-        public ObservableCollection<string> Characters { get; set; } = new ObservableCollection<string>();
-        public MainPage()
-        {
+        //public ObservableCollection<string> Characters { get; set; } = new ObservableCollection<string>();
+        public ObservableCollection<Book> ThroneBooks { get; set; } = new ObservableCollection<Book>();
+        public ObservableCollection<Character> Characters { get; set; } = new ObservableCollection<Character>();
+        public ObservableCollection<House> Houses { get; set; } = new ObservableCollection<House>();
+        public MainPage() {
             InitializeComponent();
             Categories.Add("Books");
             Categories.Add("Characters");
             Categories.Add("Houses");
 
-            Characters.Add("Jon Snow");
-            Characters.Add("Jaime Lannister");
+            //Characters.Add("Jon Snow");
+            //Characters.Add("Jaime Lannister");
 
             CategoryPicker.ItemsSource = Categories;
             MainList.ItemsSource = Characters;
         }
 
-        private void SearchTapped(object sender, EventArgs e)
-        {
-            switch ((string)CategoryPicker.SelectedItem)
-            {
+        private void SearchTapped(object sender, EventArgs e) {
+            string selectedCategory = CategoryPicker.SelectedItem.ToString();
+            object selectedItem = MainList.SelectedItem;
+            switch (selectedCategory) {
                 case "Books":
-                    Navigation.PushAsync(new BookDetails());
+                    var selectedBook = (Book)selectedItem;
+                    Navigation.PushAsync(new BookDetails(selectedBook.url));
                     break;
                 case "Characters":
                     Navigation.PushAsync(new CharacterDetails());
@@ -48,18 +51,56 @@ namespace XamarinGOT
             }
         }
 
-        private void MainListItemTapped(object sender, ItemTappedEventArgs e)
-        {
-            switch ((string)CategoryPicker.SelectedItem)
-            {
+        private void MainListItemTapped(object sender, ItemTappedEventArgs e) {
+            string selectedCategory = CategoryPicker.SelectedItem.ToString();
+            object selectedItem = MainList.SelectedItem;
+            switch (selectedCategory) {
                 case "Books":
-                    Navigation.PushAsync(new BookDetails());
+                    var selectedBook = (Book)selectedItem;
+                    Navigation.PushAsync(new BookDetails(selectedBook.url));
                     break;
                 case "Characters":
                     Navigation.PushAsync(new CharacterDetails());
                     break;
                 case "Houses":
                     Navigation.PushAsync(new HouseDetails());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private async void CategoryChanged(object sender, EventArgs e) {
+            string selectedCategory = CategoryPicker.Items[CategoryPicker.SelectedIndex];
+            var service = new GOTService();
+
+            switch (selectedCategory) {
+                case "Books":
+                    if (!ThroneBooks.Any()) {
+                        var books = await service.GetSelectedList<Book>(selectedCategory.ToLower());
+                        foreach (var item in books) {
+                            ThroneBooks.Add(item);
+                        }
+                    }
+                    MainList.ItemsSource = ThroneBooks;
+                    break;
+                case "Characters":
+                    if (!Characters.Any()) {
+                        var characters = await service.GetSelectedList<Character>(selectedCategory.ToLower());
+                        foreach (var item in characters) {
+                            Characters.Add(item);
+                        }
+                    }
+                    MainList.ItemsSource = Characters;
+                    break;
+                case "Houses":
+                    if (!Houses.Any()) {
+                        var houses = await service.GetSelectedList<House>(selectedCategory.ToLower());
+                        foreach (var item in houses) {
+                            Houses.Add(item);
+                        }
+                    }
+                    MainList.ItemsSource = Houses;
                     break;
                 default:
                     break;
