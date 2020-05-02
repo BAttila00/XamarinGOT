d4e63bff16c6b248ccfbbb2aff36506134d6d523
BAttila <attila.bruder@business-solution.hu>
2020-05-03 01:27:30 +0200

    installing sqlite-net-pcl, wpf project to build database added, saving characters to database funcionality added

---
+++ DatabaseBuilderWPF/GotDatabase.cs
@@ -0,0 +1,45 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using XamarinGOT.Models;
+
+namespace DatabaseBuilderWPF {
+    class GotDatabase {
+        readonly SQLiteConnection database;
+        public GotDatabase(string dbPath) {
+
+            database = new SQLiteConnection(dbPath);
+
+            database.CreateTable<CharacterBase>();
+
+        }
+
+        #region CharacterBase
+
+        public List<CharacterBase> GetCharacterBases() {
+            return database.Table<CharacterBase>().ToList();
+        }
+
+        public CharacterBase GetCharacterBase(int id) {
+            return database.Table<CharacterBase>()
+                            .Where(i => i.ID == id)
+                            .FirstOrDefault();
+        }
+
+        public int SaveCharacterBase(CharacterBase CharacterBase) {
+            if (CharacterBase.ID != 0) {
+                return database.Update(CharacterBase);
+            } else {
+                return database.Insert(CharacterBase);
+            }
+        }
+
+        public int DeleteCharacterBase(CharacterBase CharacterBase) {
+            return database.Delete(CharacterBase);
+        }
+
+        #endregion
+
+    }
+}
